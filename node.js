068cbd4261e47dd7a37959e0e3cce188f9f6afb8
Per Guth <mail@perguth.de>
2017-10-11 23:50:10 +0200

    node server

---
+++ node.js
@@ -0,0 +1,63 @@
+let express = require('express')
+let fetch = require('node-fetch')
+let nanobus = require('nanobus')
+let https = require('https')
+let fs = require('fs')
+
+let app = express()
+let bus = nanobus()
+let state = {}
+let v = require('./package.json').version[0]
+let {PORT, CERT, KEY} = process.env
+
+CERT = fs.readFileSync(CERT, 'utf8')
+KEY = fs.readFileSync(KEY, 'utf8')
+
+nodeStore()
+
+setInterval(x => { bus.emit('updateAll') }, 1000 * 60 * 60 * 15)
+bus.emit('updateAll')
+
+function nodeStore () {
+  state.nodes = {}
+  state.names = {}
+  bus.on('updateAll', x => {
+    fetch('https://netinfo.freifunk-stuttgart.de/json/nodes.json')
+      .then(res => res.json()).then(res => {
+        state.timestamp = res.meta.timestamp
+        res.nodes.forEach(node => {
+          state.nodes[node.id] = node
+          state.names[node.name] = node.id
+        })
+      })
+  })
+}
+
+app.get(`/version`, (req, res) => res.send(v))
+
+app.get(`/${v}/mac/:mac`, (req, res) => {
+  let node = {
+    ...state.nodes[req.params.mac],
+    timestamp: state.timestamp
+  }
+  res.send(node)
+})
+
+app.get(`/${v}/name/:name`, (req, res) => {
+  let mac = state.names[req.params.name]
+  let node = {
+    ...state.nodes[mac],
+    timestamp: state.timestamp
+  }
+  res.send(node)
+})
+
+app.get(`/${v}/all`, (req, res) => {
+  let nodes = {
+    ...state.nodes,
+    timestamp: state.timestamp
+  }
+  res.send(nodes)
+})
+
+https.createServer({CERT, KEY}, app).listen(PORT, x => { console.log(`running on :${PORT}`) })
